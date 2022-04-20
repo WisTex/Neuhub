cd9097d3c557dcd3180d970f3a20fe39f5702390
Scott M. Stolz <sstolz@wistex.com>
2022-04-20 10:33:43 -0500

    Redbasic Override 2.2

--- view/theme/redbasicoverride/php/theme.php
+++ view/theme/redbasicoverride/php/theme.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- *   * Name: Redbasic
- *   * Description: Hubzilla standard theme
+ *   * Name: Redbasic Override
+ *   * Description: Hubzilla standard theme with all tpl files
  *   * Version: 2.2
  *   * MinVersion: 6.5.13
  *   * MaxVersion: 8.0
  *   * Author: Fabrixxm
  *   * Maintainer: Mike Macgirvin
  *   * Maintainer: Mario Vavti
+ *   * Modified by: Scott M. Stolz
  *   * Theme_Color: #343a40
  *   * Background_Color: rgb(254,254,254)
  */
