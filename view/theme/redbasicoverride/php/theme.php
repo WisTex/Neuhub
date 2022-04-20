adf39008fbd1ac85e06ac68d37cdceb864b44246
Scott M. Stolz <sstolz@wistex.com>
2022-04-20 09:57:58 -0500

    Redbasic 2.2
    Red Basic Override is based on Redbasic 2.2

---
+++ view/theme/redbasicoverride/php/theme.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *   * Name: Redbasic
+ *   * Description: Hubzilla standard theme
+ *   * Version: 2.2
+ *   * MinVersion: 6.5.13
+ *   * MaxVersion: 8.0
+ *   * Author: Fabrixxm
+ *   * Maintainer: Mike Macgirvin
+ *   * Maintainer: Mario Vavti
+ *   * Theme_Color: #343a40
+ *   * Background_Color: rgb(254,254,254)
+ */
+
+function redbasic_init(&$a) {
+
+}
