a47f5bd27701a0313426992065bdaf820cec69c8
Scott M. Stolz <sstolz@wistex.com>
2022-04-21 22:05:36 -0500

    Sunbeam 1.0
    The original Sunbeam theme did not have a version number, so I am just arbitrarily calling it version 1.0.

---
+++ view/theme/sunbeam/php/theme.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *   * Name: Sunbeam
+ *   * Description: A fresh theme based on redbasic
+ *   * MinVersion: 2.3.1
+ *   * MaxVersion: 4.0
+ *   * Author: Sean Tilley
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function sunbeam_init(&$App) {
+
+    App::$theme_info['extends'] = 'redbasic';
+
+
+}
