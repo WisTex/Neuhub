4b200242ef9dc75f34441dddd360515787025580
Scott M. Stolz <sstolz@wistex.com>
2022-10-18 05:37:15 -0500

    Remove Old Links
    Cleaned up a bit, removing references to files we don't use anymore.

--- view/theme/neuhub-red-dash/php/theme_init.php
+++ view/theme/neuhub-red-dash/php/theme_init.php
@@ -3,11 +3,6 @@
 // Boostrap CSS
 head_add_css('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css');
 head_add_css('/view/theme/neuhub-red-dash/assets/bootstrap/css/bootstrap.min.css');
-// head_add_css('/view/theme/purplebasic/assets/bootstrap-5.2.0-beta1/dist/css/bootstrap.css');
-
-// head_add_css('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css');
-
-// head_add_css('/view/theme/neuhub-red-dash/assets/bootstrap-5.0.2-dist/css/bootstrap.css');
 
 head_add_css('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');
 
@@ -32,6 +27,7 @@ require_once('view/php/theme_init.php');
 head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
 // head_add_js('/view/theme/neuhub-red-dash/assets/bootstrap/js/bootstrap.min.js');
 
+// Other Javascript
 head_add_js('/library/bootbox/bootbox.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
 head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
