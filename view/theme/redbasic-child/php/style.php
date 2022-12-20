33580731d5cd5f26c6cb5b60992781090098a92b
Scott M. Stolz <sstolz@wistex.com>
2022-12-20 16:54:00 -0600

    Redbasic Child Theme Initial Commit

---
+++ view/theme/redbasic-child/php/style.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once('view/theme/redbasic/php/config.php');
+require_once('view/theme/redbasic/php/style.php');
+
+echo @file_get_contents('view/theme/redbasic-child/css/style.css');
---
+++ view/theme/redbasic-child/php/theme.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *   * Name: Redbasic Child Theme
+ *   * Description: A derivative theme of Redbasic that you can use as a starting point for creating your own themes.
+ *   * Version: 1.2
+ *   * MinVersion: 7.2
+ *   * MaxVersion: 10.0
+ *   * Author: Scott M. Stolz
+ *   * Maintainer: Scott M. Stolz
+ *   * License: MIT License (Expat Version) - https://license.neuhub.org
+ *   * Copyright: 2022 © WisTex TechSero Ltd. Co.
+ *   * Respository: https://github.com/WisTex/Neuhub
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function redbasicchild_init(&$App) {
+
+    App::$theme_info['extends'] = 'redbasic';
+
+
+}
