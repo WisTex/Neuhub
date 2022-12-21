7ff2194a3a38485a9cf569f8796c0cf2b19f9077
Scott M. Stolz <sstolz@wistex.com>
2022-12-21 00:37:21 -0600

    Updated Instructions

--- view/theme/redbasic-child/php/style.php
+++ view/theme/redbasic-child/php/style.php
@@ -1,7 +1,10 @@
 <?php
 
+// This pulls the default config and styles from Redbasic.
+// This allows your child theme to only specify changes. Anything undefined is pulled from Redbasic.
+// No need to change these two lines unless you are completely rewriting the theme.
 require_once('view/theme/redbasic/php/config.php');
 require_once('view/theme/redbasic/php/style.php');
 
-// If you change the name of the directory containing the theme, be sure to change this line to match.
+// ! If you change the name of the directory containing the theme, be sure to change this line to match.
 echo @file_get_contents('view/theme/redbasic-child/css/style.css');
--- view/theme/redbasic-child/php/theme.php
+++ view/theme/redbasic-child/php/theme.php
@@ -15,6 +15,7 @@
  *
  */
 
+// When you create a new theme, don't forget to edit the information above.
 // If you change the name of the theme to `yournewname` change `redbasicchild_init` to `yournewname_init` so it has a unique name.
 // You will also need to edit the style.php file if you change the directory name.
 
