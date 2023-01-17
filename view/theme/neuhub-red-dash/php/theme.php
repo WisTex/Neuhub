298fb82639835625abf2a614f18d89cab200af33
Scott M. Stolz <sstolz@wistex.com>
2023-01-17 05:06:50 -0600

    Update Version Number

--- view/theme/neuhub-red-dash/php/theme.php
+++ view/theme/neuhub-red-dash/php/theme.php
@@ -3,8 +3,8 @@
 /**
  *   * Name: Neuhub Red Dash
  *   * Description: A proof of concept theme combining Redbasic with SB Admin as the base.
- *   * Version: 0.6.4
- *   * MinVersion: 6.5.13
+ *   * Version: 0.7
+ *   * MinVersion: 8.0
  *   * MaxVersion: 10.0
  *   * Author: Scott M. Stolz
  *   * Maintainer: Scott M. Stolz
