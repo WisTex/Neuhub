ea1f5278acce9b221b7bb97403081913a83610ee
Scott M. Stolz <sstolz@wistex.com>
2023-08-11 01:59:54 -0500

    User Tabler's Bootstrap
    User Tabler's Bootstrap instead of using Red Basic's Bootstrap. You can't use both JS, since they conflict.

--- view/theme/red-tabler/php/theme_init.php
+++ view/theme/red-tabler/php/theme_init.php
@@ -2,7 +2,7 @@
 
 // Add your custom CSS files here.
 head_add_css('/library/fork-awesome/css/fork-awesome.min.css');
-head_add_css('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css');
+// head_add_css('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css'); // Tabler already uses Bootstrap, so this would conflict with it.
 head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
 head_add_css('/view/css/bootstrap-red.css');
 head_add_css('/library/datetimepicker/jquery.datetimepicker.css');
@@ -12,7 +12,7 @@ head_add_css('/view/css/default.css');
 require_once('view/php/theme_init.php');
 
 // Add your custom JS files here.
-head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js');
+// head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js'); // Tabler already uses Bootstrap, so this would conflict with it.
 head_add_js('/library/bootbox/bootbox.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
 head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
