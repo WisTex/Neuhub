28a7a0d3072bd35fbee2177d4f99ab4f81d177fc
Scott M. Stolz <sstolz@wistex.com>
2023-08-16 22:17:18 -0500

    Added Comments

--- view/theme/red-tab/php/theme_init.php
+++ view/theme/red-tab/php/theme_init.php
@@ -1,6 +1,8 @@
 <?php
 
-// Add your custom CSS files here.
+// ------------------------------------------
+// SECTION I: Add your custom CSS files here.
+
 head_add_css('/library/fork-awesome/css/fork-awesome.min.css');
 // head_add_css('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css'); // Tabler already uses Bootstrap, so this would conflict with it.
 head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
@@ -9,16 +11,19 @@ head_add_css('/library/datetimepicker/jquery.datetimepicker.css');
 head_add_css('/library/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css');
 head_add_css('/view/css/default.css');
 
+// This calls Redbasic's default CSS and JS files.
 require_once('view/php/theme_init.php');
 
+//These are the Tabler UI Kit's CSS files used in this theme.
 head_add_css('/view/theme/red-tab/dist/css/tabler.min.css');
 head_add_css('/view/theme/red-tab/dist/css/tabler-flags.min.css');
 head_add_css('/view/theme/red-tab/dist/css/tabler-payments.min.css');
 head_add_css('/view/theme/red-tab/dist/css/tabler-vendors.min.css');
 head_add_css('/view/theme/red-tab/dist/css/demo.min.css');
 
+// ------------------------------------------
+// SECTION II: Add your custom JS files here.
 
-// Add your custom JS files here.
 // head_add_js('/vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js'); // Tabler already uses Bootstrap, so this would conflict with it.
 head_add_js('/library/bootbox/bootbox.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
@@ -26,6 +31,5 @@ head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
 head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
 head_add_js('/view/theme/redbasic/js/redbasic.js');
 
+//These are the Tabler UI Kit's JS files used in this theme.
 head_add_js('/view/theme/red-tab/dist/js/demo-theme.min.js');
-
-
